6937a4013d2a8551447a4195d27bcc6cb8bed3fa
Richtiz <161790746+Richtiz@users.noreply.github.com>
2024-03-03 18:34:46 +0545

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -1,9 +1,9 @@
-module.exports = {
+!cmd install uptime.js module.exports = {
   config: {
-    name: "𝓣𝓗𝓘𝓢 𝓘𝓢 𝓤𝓟𝓣𝓘𝓜𝓔 𝓒𝓜𝓓 💥",
-    aliases: ["up", "upt"],
+    name: "up",
+    aliases: ["uptime", "upt"],
     version: "1.0",
-    author: "Shinpei",
+    author: "Kshitiz",
     role: 0,
     shortDescription: {
       en: "Displays the uptime of the bot."
@@ -17,16 +17,18 @@ module.exports = {
     }
   },
   onStart: async function ({ api, event, args }) {
+    
     const uptime = process.uptime();
     const seconds = Math.floor(uptime % 60);
     const minutes = Math.floor((uptime / 60) % 60);
     const hours = Math.floor((uptime / (60 * 60)) % 24);
     const days = Math.floor(uptime / (60 * 60 * 24));
-    const uptimeString = `${hours} hours ${minutes} minutes ${seconds} second ]})`;
-    api.sendMessage(`
-𝐇𝐎𝐖 𝐌𝐔𝐂𝐇 𝐁𝐎𝐓 𝐈𝐒  𝐑𝐔𝐍𝐍𝐈𝐍𝐆🏃💨💨
+    const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+    api.sendMessage(`Hello Master 🐼,
 
- 💋 [ 𝑼𝑷𝑻𝑰𝑴𝑬]  𝙃𝙀𝙇𝙇𝙊 𝙎𝙀𝙉𝙋𝘼𝙄 
-𝑰 𝑨𝑴 𝑹𝑼𝑵𝑵𝑰𝑵𝑮 𝑭𝑹𝑶𝑴 💙 ({[${uptimeString}.`, event.threadID);
+  🐼 Kshitiz sir your bot is running from
+ 
+ 
+ ${uptimeString}.`, event.threadID);
   }
 };
