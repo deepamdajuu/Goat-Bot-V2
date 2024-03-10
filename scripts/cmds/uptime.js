edfec4a2871f3ca550af19312a8daae7fb7f1c9e
Richtiz <161790746+Richtiz@users.noreply.github.com>
2024-03-10 16:59:18 +0545

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -24,7 +24,7 @@ module.exports = {
     const hours = Math.floor((uptime / (60 * 60)) % 24);
     const days = Math.floor(uptime / (60 * 60 * 24));
     const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
-    api.sendMessage(`Hello Master 🐼,
+    api.sendMessage(`What's up🐼,
 
   🐼 Kshitiz sir your bot is running from
  
